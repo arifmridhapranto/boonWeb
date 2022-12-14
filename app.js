6e0bb09ca5a389b6742f66706ae3489d8b3343b4
Arif Mridha Pranto <mridhapranto647@gmail.com>
2022-12-14 16:13:12 +0600

    service page is added and complete

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
+jQuery(document).ready(function ($) {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    items: 1,
+    dots: false,
+  });
 
-jQuery(".owl-carousel").owlCarousel({
-  loop: true,
-  margin: 0,
-  nav: true,
-  items: 1,
-  dots: false,
-});
-
-jQuery("#service-slider").owlCarousel({
+  $("#service-slider").owlCarousel({
     loop: true,
     margin: 0,
     nav: true,
@@ -15,22 +15,26 @@ jQuery("#service-slider").owlCarousel({
     nav: true,
     dots: false,
     center: true,
-});
-const work_slide = document.getElementById("works-slider");
-work_slide.owlCarousel({
-  loop: true,
-  items: 1,
-  margin: 0,
-  dots: false,
-  nav: true,
-  smartSpeed: 450,
-  navText: [
-    '<i class="fa fa-angle-left"></i>',
-    '<i class="fa fa-angle-right"></i>',
-  ],
-});
+  });
+  const work_slide = document.getElementById("works-slider");
+  work_slide.owlCarousel({
+    loop: true,
+    items: 1,
+    margin: 0,
+    dots: false,
+    nav: true,
+    smartSpeed: 450,
+    navText: [
+      '<i class="fa fa-angle-left"></i>',
+      '<i class="fa fa-angle-right"></i>',
+    ],
+  });
 
 
-function toggleMobileMenu(menu) {
-  menu.classList.toggle("open");
-}
+  function toggleMobileMenu(menu) {
+    menu.classList.toggle("open");
+  }
+
+
+  $(".counter").counterUp();
+});
